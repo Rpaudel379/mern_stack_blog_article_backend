93caf9409d00fe2b960a616962f67dc32845df89
Anish Paudel <rpaudel379@gmail.com>
2023-06-19 16:15:56 +0530

    first commit

--- src/index.ts
+++ src/index.ts
@@ -39,6 +39,7 @@ const port = process.env.PORT;
 
 mongoose.connect(process.env.MONGO_URL!).then(() => {
   console.log("database connected");
+  
   app.listen(port, () => {
     console.log(`Server running on port ${port}`);
   });
