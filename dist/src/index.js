fab4ff49e9a6056a9b8ba4345022060d203a6c6d
Anish Paudel <rpaudel379@gmail.com>
2023-06-24 21:39:00 +0530

    chan

--- dist/src/index.js
+++ dist/src/index.js
@@ -17,7 +17,7 @@ dotenv_1.default.config();
 // app.use(cors());
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
-app.use((0, cors_1.default)());
+app.use((0, cors_1.default)({ origin: process.env.frontend, credentials: true }));
 app.use((0, cookie_parser_1.default)());
 //? TODO disable if not sent request from API
 //? TODO disable if not sent request from API
--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ dotenv.config();
 // app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({ origin: process.env.frontend, credentials: true }));
 
 app.use(cookieParser());
 //? TODO disable if not sent request from API
