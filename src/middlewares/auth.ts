7f0336fbe6a2ee9d49ee58fe45dce08467aeff93
Anish Paudel <rpaudel379@gmail.com>
2023-06-28 18:43:41 +0530

    properly typed middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,26 +1,29 @@
 import { NextFunction, Request, Response } from "express";
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload, Secret } from "jsonwebtoken";
+import User from "@models/User";
 
-export const isLoggedIn = (req: Request, res: Response, next: NextFunction) => {
+export const isLoggedIn = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   // const token = req.cookies.auth;
   const token = req.headers.authorization?.split(" ")[1];
 
   if (token) {
-    jwt.verify(
-      token,
-      process.env.JWT_KEY as string,
-      async (err: any, decodedToken: any) => {
-        if (err) {
-          return res.status(401).json({ message: "Invalid Token" });
-        } else {
-          res.locals.user = decodedToken;
-          next();
-        }
-      }
-    );
+    try {
+      const decodedToken = jwt.verify(
+        token,
+        process.env.JWT_KEY as Secret
+      ) as JwtPayload;
+
+      // throws an error if decodedToken.id is not the objectId
+      await User.findById(decodedToken.id);
+      next();
+    } catch (error) {
+      return res.status(401).json({ message: "Invalid token" });
+    }
   } else {
     return res.status(401).json({ message: "User not logged in" });
-    // throw new Error("401=User not logged in");
-    // res.status(400).json({ validation: false, message: "User not logged in" });
   }
 };
